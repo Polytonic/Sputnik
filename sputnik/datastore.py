daaac7ffb3bab438476d5db20d2aae1221444d65
RandomGuy928 <randomguy928@gmail.com>
2014-11-18 15:24:34 -0500

    Initial code review tweaks

--- sputnik/datastore.py
+++ sputnik/datastore.py
@@ -1,6 +1,6 @@
-"""Sputnik DataStore Implementation
+"""Sputnik Datastore Implementation
 
-This module provides the Sputnik DataStore implementation. It defines the 
+This module provides the Sputnik Datastore implementation. It defines the 
 behaviors necessary to store and reload channels and networks should the bouncer ever
 unexpectedly go offline.
 """
@@ -8,10 +8,10 @@ unexpectedly go offline.
 import redis
 import json
 
-class DataStore(object):
+class Datastore(object):
     """A singleton that stores all channels and networks connected to by the bouncer.
 
-    The DataStore is a feature that stores channel and network information in a database
+    The Datastore is a feature that stores channel and network information in a database
     so that the bouncer can automatically reconnect to channels and networks in the event
     that the bouncer unexpectedly goes offline and must be restarted.  The primary function
     of this class is to act as a write-through cache for channel and network information
@@ -26,41 +26,46 @@ class DataStore(object):
             value = {hostname':'<hostname>', 'port':<port>, 'nickname':'<nickname>',
                     'username':'<username>', 'realname':'<realname>', 'password':'<password>',
                     'usermode':<usermode>}
-        _database (redis database): A redis database session.
+        database (redis database): A redis database session.
     """
-    def __init__(self):
-        """Creates an instance of a DataStore.
+    def __init__(self, hostname="localhost", port=6379):
+        """Creates an instance of a Datastore.
 
         Initializes an empty dictionary of channel info and populates it with
         entries stored in the database.
+
+        Args:
+            hostname (str): The hostname for the redis database.
+            port (int): The port for the redis database.
         """
 
         self.channels = {}
         self.networks = {}
-        self._database = redis.StrictRedis(host='localhost', port=6379, db=0)
-        self._load_from_database()
+        self.database = redis.StrictRedis(host=hostname, port=port, db=0)
+        self.load_from_database()
 
-    def _load_from_database(self):
-        """Loads info stored in the database into the DataStore's dictionaries.
+    def load_from_database(self):
+        """Loads info stored in the database into the Datastore's dictionaries.
         """
 
-        for key in self._database.keys("*"):
+        for key in self.database.keys():
             decoded_key = key.decode()
 
             #If this is a channel
             if ":" in decoded_key:
-                password = self._database.get(key).decode()
-                if password == "":
+                password = self.database.get(key).decode()
+                if not password:
                     password = None
                 self.channels[decoded_key] = password
 
             #If this is a network
             else:
-                json_val = self._database.get(key).decode()
+                json_val = self.database.get(key).decode()
                 val = json.loads(json_val)
                 self.networks[decoded_key] = val
 
-    def add_network(self, network, hostname, port, nickname, username, realname, password = None, usermode = 0):
+    def add_network(self, network, hostname, port, nickname, username, realname,
+                    password=None, usermode=0):
         """Adds a network to the networks dictionary and underlying database.
 
         Args:
@@ -79,7 +84,7 @@ class DataStore(object):
                 "usermode":usermode}
         self.networks[network] = val
         json_val = json.dumps(val)
-        self._database.set(network, json_val)
+        self.database.set(network, json_val)
 
     def remove_network(self, network):
         """Removes a network from the networks dictionary and underlying database.
@@ -89,7 +94,7 @@ class DataStore(object):
         """
 
         self.networks.pop(network, None)
-        self._database.delete(network)
+        self.database.delete(network)
 
     def add_channel(self, network, channel, password = None):
         """Adds a channel to the channels dictionary and underlying database.
@@ -102,9 +107,9 @@ class DataStore(object):
 
         key = ":".join([network, channel])
         self.channels[key] = password
-        if password is None:
+        if not password:
             password = ""
-        self._database.set(key, password)
+        self.database.set(key, password)
 
     def remove_channel(self, network, channel):
         """Removes a channel from the channel dictionary and underlying database.
@@ -116,4 +121,4 @@ class DataStore(object):
 
         key = ":".join([network, channel])
         self.channels.pop(key, None)
-        self._database.delete(key)
+        self.database.delete(key)
