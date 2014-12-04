662e87039405b6375df458a7e511ab7fcb22ef6c
Patrick <biernp@rpi.edu>
2014-12-03 21:19:18 -0500

    Alphabet yo

--- sputnik/bouncer.py
+++ sputnik/bouncer.py
@@ -5,12 +5,12 @@ point, the Bouncer is responsible for bootstrapping the entire program.
 """
 
 import asyncio
+import os
 import redis
 from client import Client
 from datastore import Datastore
 from network import Network
 from server import HTTPServer
-import os
 
 class Bouncer(object):
     """A singleton that manages connected devices.
